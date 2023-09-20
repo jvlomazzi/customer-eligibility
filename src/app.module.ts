4e3bb2eacaa73d78ba79bc697a7de3dd3572b164
João Matos <41305642+jvlomazzi@users.noreply.github.com>
2023-09-20 17:56:22 -0300

    :fire: sanitize code base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ResourcesModule } from './resources/resources.module';
 
@@ -12,27 +9,8 @@ import { ResourcesModule } from './resources/resources.module';
       isGlobal: true,
       envFilePath: ['.env'],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'), // Use o ConfigService para obter configurações
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [`${__dirname}/resources/*/entities/*.entity{.ts,.js}`],
-        migrations: [`${__dirname}/migrations/*{.ts,.js}`],
-        migrationsTableName: 'migrations',
-        migrationsRun: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
     ResourcesModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/resources/customers/customers.controller.ts
+++ src/resources/customers/customers.controller.ts
@@ -1,16 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CustomersService } from './customers.service';
-import { CreateCustomerDto } from './dto/create-customer.dto';
-import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { CheckEligibilityDto } from './dto/check-eligibility.dto';
 
 @ApiTags('Customers')
 @Controller('customers')
@@ -18,30 +14,14 @@ export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
   @Post()
-  create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customersService.create(createCustomerDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.customersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.customersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCustomerDto: UpdateCustomerDto,
-  ) {
-    return this.customersService.update(+id, updateCustomerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customersService.remove(+id);
+  @ApiOperation({ summary: 'Check customer eligibity' })
+  @ApiOkResponse({
+    type: CheckEligibilityDto,
+    description: 'Return the customer eligibility',
+    isArray: false,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  checkEligibility(@Body() dto: CheckEligibilityDto) {
+    return this.customersService.checkEligibility(dto);
   }
 }
